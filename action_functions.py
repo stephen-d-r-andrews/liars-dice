2f17d0ebd81a121bea228c132ec5b1a0786b1753
Stephen Andrews <stephen.d.r.andrews@gmail.com>
2024-07-04 22:38:45 -0400

    necessary functions needed to be implemented

---
+++ action_functions.py
@@ -0,0 +1,23 @@
+def get_action_1(history, hand):
+    """
+    Gets player 1's choice of move based on history of game and their hand
+    """
+    pass
+
+def get_action_2(history):
+    """
+    Gets player 2's choice of move based on history of game and theur hand
+    """
+    pass
+
+def check_stronger(claim1, claim2):
+    """
+    Checks which hand is stronger
+    """
+    pass
+
+def get_strongest(p1hand, p2hand):
+    """
+    Gets strongest claim based on the 4 cards
+    """
+    pass
