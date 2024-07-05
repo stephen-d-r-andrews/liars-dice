51c790f302d69f7939b6fc3ca864f02f0a2a8d22
Stephen Andrews <stephen.d.r.andrews@gmail.com>
2024-07-04 22:51:18 -0400

    Adds some detail to get_action_1 for dealing with the first move

--- action_functions.py
+++ action_functions.py
@@ -2,9 +2,14 @@ def get_action_1(history, hand):
     """
     Gets player 1's choice of move based on history of game and their hand
     """
-    pass
+    if not history[0] and not history[1]: # beginning of the game
+        pass
 
-def get_action_2(history):
+    else:
+        prev_action = history[1][-1]
+
+
+def get_action_2(history, hand):
     """
     Gets player 2's choice of move based on history of game and theur hand
     """
@@ -13,5 +18,11 @@ def get_action_2(history):
 def check_exists(claim1, p1_hand, p2_hand):
     """
     Checks if the claim is True or not
+    """
+    pass
+
+def check_stronger(claim1, claim2):
+    """
+    Checks which hand is stronger
     """
     pass
